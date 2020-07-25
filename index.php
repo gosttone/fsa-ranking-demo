94556d2f5fbacc59aa623d11d7fa9645c968db31
Gerson Toledo <gotalmoe@hotmail.com>
2020-07-24 20:10:38 -0600

    Actualizacion
    Ranking actualizado al 24/07/2020

--- index.php
+++ index.php
@@ -26,235 +26,229 @@
 
                     <tr>
                         <th scope="row">1</th>
-                        <td>2 Dan</td>
+                        <td>2 dan</td>
                         <td>chilenito</td>
                         <td><span class="flag-icon flag-icon-cl flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">2</th>
-                        <td>1 Dan</td>
+                        <td>1 dan</td>
                         <td>kasparjan</td>
                         <td><span class="flag-icon flag-icon-ar flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">3</th>
-                        <td>1 Dan</td>
+                        <td>1 dan</td>
                         <td>minikt23</td>
                         <td><span class="flag-icon flag-icon-es flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">4</th>
-                        <td>5 Kyu</td>
+                        <td>5 kyu</td>
                         <td>rumpel</td>
                         <td><span class="flag-icon flag-icon-pe flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">5</th>
-                        <td>3 Kyu</td>
+                        <td>3 kyu</td>
                         <td>gerarduv</td>
                         <td><span class="flag-icon flag-icon-mx flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">6</th>
-                        <td>4 Kyu</td>
+                        <td>4 kyu</td>
                         <td>emperor</td>
                         <td><span class="flag-icon flag-icon-cl flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">7</th>
-                        <td>4 Kyu</td>
+                        <td>4 kyu</td>
                         <td>judagar</td>
                         <td><span class="flag-icon flag-icon-co flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">8</th>
-                        <td>4 Kyu</td>
+                        <td>4 kyu</td>
                         <td>shymon</td>
                         <td><span class="flag-icon flag-icon-us flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">9</th>
-                        <td>5 Kyu</td>
+                        <td>5 kyu</td>
                         <td>alfieri</td>
                         <td><span class="flag-icon flag-icon-pe flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">10</th>
-                        <td>7 Kyu</td>
-                        <td>sir gandalf</td>
-                        <td><span class="flag-icon flag-icon-cl flag-icon-squared"></span></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">11</th>
                         <td>4 kyu</td>
                         <td>dan</td>
                         <td><span class="flag-icon flag-icon-mx flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
-                        <th scope="row">12</th>
-                        <td>7 Kyu</td>
-                        <td>aniquil</td>
-                        <td><span class="flag-icon flag-icon-mx flag-icon-squared"></span></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">13</th>
-                        <td>7 Kyu</td>
-                        <td>hayama</td>
+                                        <tr>
+                        <th scope="row">11</th>
+                        <td>7 kyu</td>
+                        <td>sir gandalf</td>
                         <td><span class="flag-icon flag-icon-cl flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
-                        <th scope="row">14</th>
-                        <td>5 Kyu</td>
+                                        <tr>
+                        <th scope="row">12</th>
+                        <td>5 kyu</td>
                         <td>the pharmacist</td>
                         <td><span class="flag-icon flag-icon-gt flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
+                        <th scope="row">13</th>
+                        <td>7 kyu</td>
+                        <td>aniquil</td>
+                        <td><span class="flag-icon flag-icon-mx flag-icon-squared"></span></td>
+                    </tr>
+                                        <tr>
+                        <th scope="row">14</th>
+                        <td>7 kyu</td>
+                        <td>hayama</td>
+                        <td><span class="flag-icon flag-icon-cl flag-icon-squared"></span></td>
+                    </tr>
+                                        <tr>
                         <th scope="row">15</th>
-                        <td>12 Kyu</td>
-                        <td>sanosuke</td>
-                        <td><span class="flag-icon flag-icon-ar flag-icon-squared"></span></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">16</th>
-                        <td>7 Kyu</td>
-                        <td>kurahasho</td>
-                        <td><span class="flag-icon flag-icon-co flag-icon-squared"></span></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">17</th>
-                        <td>9 Kyu</td>
+                        <td>9 kyu</td>
                         <td>keinny</td>
                         <td><span class="flag-icon flag-icon-ve flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
+                        <th scope="row">16</th>
+                        <td>12 kyu</td>
+                        <td>sanosuke</td>
+                        <td><span class="flag-icon flag-icon-ar flag-icon-squared"></span></td>
+                    </tr>
+                                        <tr>
+                        <th scope="row">17</th>
+                        <td>7 kyu</td>
+                        <td>kurahasho</td>
+                        <td><span class="flag-icon flag-icon-co flag-icon-squared"></span></td>
+                    </tr>
+                                        <tr>
                         <th scope="row">18</th>
-                        <td>9 Kyu</td>
+                        <td>9 kyu</td>
                         <td>logus</td>
                         <td><span class="flag-icon flag-icon-mx flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">19</th>
-                        <td>9 Kyu</td>
+                        <td>9 kyu</td>
                         <td>soul nova</td>
                         <td><span class="flag-icon flag-icon-mx flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">20</th>
-                        <td>10 Kyu</td>
+                        <td>10 kyu</td>
                         <td>asc</td>
                         <td><span class="flag-icon flag-icon-mx flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">21</th>
-                        <td>9 Kyu</td>
+                        <td>9 kyu</td>
                         <td>Luciano</td>
                         <td><span class="flag-icon flag-icon-br flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">22</th>
-                        <td>10 Kyu</td>
+                        <td>10 kyu</td>
                         <td>ctr</td>
                         <td><span class="flag-icon flag-icon-pe flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">23</th>
-                        <td>10 Kyu</td>
+                        <td>10 kyu</td>
                         <td>quetzal</td>
                         <td><span class="flag-icon flag-icon-gt flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">24</th>
-                        <td>11 Kyu</td>
+                        <td>11 kyu</td>
                         <td>julian</td>
                         <td><span class="flag-icon flag-icon-co flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">25</th>
-                        <td>11 Kyu</td>
+                        <td>11 kyu</td>
                         <td>esther</td>
                         <td><span class="flag-icon flag-icon-mx flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">26</th>
-                        <td>9 Kyu</td>
-                        <td>julii</td>
-                        <td><span class="flag-icon flag-icon-do flag-icon-squared"></span></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">27</th>
-                        <td>10 Kyu</td>
+                        <td>10 kyu</td>
                         <td>gosttone</td>
                         <td><span class="flag-icon flag-icon-gt flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
+                        <th scope="row">27</th>
+                        <td>9 kyu</td>
+                        <td>julii</td>
+                        <td><span class="flag-icon flag-icon-do flag-icon-squared"></span></td>
+                    </tr>
+                                        <tr>
                         <th scope="row">28</th>
-                        <td>14 Kyu</td>
+                        <td>14 kyu</td>
                         <td>King_raven</td>
                         <td><span class="flag-icon flag-icon-co flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">29</th>
-                        <td>12 Kyu</td>
+                        <td>12 kyu</td>
                         <td>aurora</td>
                         <td><span class="flag-icon flag-icon-cl flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">30</th>
-                        <td>10 Kyu</td>
+                        <td>9 kyu</td>
                         <td>sebasanguiano</td>
                         <td><span class="flag-icon flag-icon-ar flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">31</th>
-                        <td>12 Kyu</td>
+                        <td>12 kyu</td>
                         <td>fabi</td>
                         <td><span class="flag-icon flag-icon-cl flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">32</th>
-                        <td>14 Kyu</td>
-                        <td>Tekladista</td>
+                        <td>14 kyu</td>
+                        <td>tekladista</td>
                         <td><span class="flag-icon flag-icon-cl flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">33</th>
-                        <td>12 Kyu</td>
+                        <td>12 kyu</td>
                         <td>katherine</td>
                         <td><span class="flag-icon flag-icon-cl flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">34</th>
-                        <td>13 Kyu</td>
+                        <td>13 kyu</td>
                         <td>yanina</td>
                         <td><span class="flag-icon flag-icon-cl flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">35</th>
-                        <td>14 Kyu</td>
+                        <td>14 kyu</td>
                         <td>medina</td>
                         <td><span class="flag-icon flag-icon-hn flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">36</th>
-                        <td>14 Kyu</td>
+                        <td>14 kyu</td>
                         <td>elcono</td>
                         <td><span class="flag-icon flag-icon-ar flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
+                                        <tr>
                         <th scope="row">37</th>
-                        <td>14 Kyu</td>
-                        <td>dendeiform</td>
-                        <td><span class="flag-icon flag-icon-cl flag-icon-squared"></span></td>
-                    </tr>
-                    <tr>
-                        <th scope="row">38</th>
-                        <td>14 Kyu</td>
+                        <td>14 kyu</td>
                         <td>snorlax</td>
                         <td><span class="flag-icon flag-icon-ar flag-icon-squared"></span></td>
                     </tr>
-                    <tr>
-                        <th scope="row">39</th>
-                        <td>14 Kyu</td>
+                                        <tr>
+                        <th scope="row">38</th>
+                        <td>14 kyu</td>
                         <td>andrew</td>
                         <td><span class="flag-icon flag-icon-pe flag-icon-squared"></span></td>
                     </tr>
